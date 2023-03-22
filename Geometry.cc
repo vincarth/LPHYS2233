80de62989cab9a8ed7eb437a239fa1319bf051a7
vincarth <74768548+vincarth@users.noreply.github.com>
2023-03-22 11:29:36 +0100

    Update Geometry.cc

--- Geometry.cc
+++ Geometry.cc
@@ -206,16 +206,16 @@ G4VPhysicalVolume* DetectorConstruction::DefineVolumes()
 
     auto Panel1LV
     = new G4LogicalVolume(
-                 Scintillator,
+                 Panel1,
 //                 layerS,           // its solid
                  defaultMaterial,  // its material
-                 "Scintillator");         // its name
+                 "Panel1);         // its name
 
     
   new G4PVPlacement(nullptr,  // no rotation
     G4ThreeVector(),          // at (0,0,0)
-    ScintillatorLV,                  // its logical volume
-    "Scintillator",            // its name
+    Panel1LV,                  // its logical volume
+    "Panel1",            // its name
     worldLV,                  // its mother  volume
     false,                    // no boolean operation
     0,                        // copy number
@@ -223,9 +223,9 @@ G4VPhysicalVolume* DetectorConstruction::DefineVolumes()
   
 
   new G4PVReplica(
-                 "Scintillator",          // its name
-                 layerScintillatorLV,          // its logical volume
-                 ScintillatorLV,          // its mother
+                 "Panel1",          // its name
+                 SlabLV,          // its logical volume
+                 Panel1LV,          // its mother
                  kYAxis,           // axis of replication
                  fNofSlab,        // number of replica
                  SlabWidth);  // witdth of replica
